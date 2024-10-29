17b09bac96d559f65efd2510fc48135a8a102ee6
Kevin Chang <kevin0216@kevin0216.us>
2024-10-30 04:48:43 +0800

    refactor: moved sessionmaker to services.py

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,13 +1,10 @@
-from sqlalchemy.orm import sessionmaker
 from openai import OpenAI, APIError, RateLimitError
 from typing import List, Dict, Optional, Union
 import itertools
 
 from .config import Config
-from .database import db_engine
 
 _id_counter = itertools.count(start=1000000)
-session = sessionmaker(bind=db_engine)
 
 openai_client = OpenAI(api_key=Config.OpenAI.OPENAI_TOKEN)
 
