6508b549a0f76898f0f309eabeda3d6d64e615d0
Kevin Chang <kevin0216@kevin0216.us>
2024-12-19 00:06:02 +0800

    refactor: added error handling mechanism

--- backend/src/users/router.py
+++ backend/src/users/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 from sqlalchemy.orm import Session
+from sentry_sdk import capture_exception
+import logging
 
 from ..config import Config
 from ..auth.dependencies import session_opener, authenticate_user_token
@@ -21,22 +23,40 @@ async def login_for_access_token(
         form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(session_opener)
 ):
     """login"""
+    logging.debug(f"Login initialised: {form_data.username}")
     user = check_user_password_is_correct(db, form_data.username, form_data.password)
-    access_token = create_access_token(
-        data={"sub": str(user.username)}, valid_duration=timedelta(minutes=Config.Auth.ACCESS_TOKEN_EXPIRE_MINUTES)
-    )
+    if not user:
+        logging.debug(f"Failed to login: {form_data.username}")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    try:
+        access_token = create_access_token(
+            data={"sub": str(user.username)}, valid_duration=timedelta(minutes=Config.Auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+        )
+    except Exception as e:
+        capture_exception(e)
+        return HTTPException(status_code=400, detail="Something went wrong while processing the request")
+    logging.debug(f"Logged in: {form_data.username}")
     return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/register")
 def create_user(user: UserAuthSchema, db: Session = Depends(session_opener)):
     """create users"""
+    logging.debug(f"Creating user: {user.username}")
     hashed_password = password_context.hash(user.password)
     db_user = User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
-    db.commit()
+    try:
+        db.commit()
+    except Exception as e:
+        logging.warning(f"Failed to create user: {user.username}, {e}, skipping.")
+        capture_exception(e)
+        db.rollback()
+        return HTTPException(status_code=400, detail="Failed to create user")
     db.refresh(db_user)
+    logging.debug(f"Created user: {user.username}")
     return db_user
 
 @router.get("/me")
 def read_users_me(user=Depends(authenticate_user_token)):
+    logging.debug(f"Accessed /api/v1/users/me: {user.username}")
     return {"username": user.username}
