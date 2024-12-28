e88794e9a7f151f5db8dca5e5151d29093b3cea3
Kevin Chang <kevin0216@kevin0216.us>
2024-12-29 04:08:35 +0800

    fix(tests): fixing error caused by base class renaming

--- backend/tests/crawler/test_udn_news_crawler.py
+++ backend/tests/crawler/test_udn_news_crawler.py
@@ -58,7 +58,7 @@ class TestUDNCrawler(unittest.TestCase):
         """
         mock_get.return_value = mock_response
 
-        news = self.scraper.parse("https://udn.com/news/test-news")
+        news = self.scraper.validate_and_parse("https://udn.com/news/test-news")
         self.assertEqual(news.title, "Test Title")
         self.assertEqual(news.time, "2023-09-08T00:00:00")
         self.assertEqual(news.content, "Content paragraph 1. Content paragraph 2.")
@@ -98,7 +98,7 @@ class TestUDNCrawler(unittest.TestCase):
     def test_parse_invalid_domain(self):
         invalid_url = "https://example.com/news/test-news"
         with self.assertRaises(DomainMismatchException):
-            self.scraper.parse(invalid_url)
+            self.scraper.validate_and_parse(invalid_url)
 
 
 if __name__ == "__main__":
